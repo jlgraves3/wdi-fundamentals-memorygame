12f2daa21471e167effbee22c2ad490ac16c9b58
Julia Graves <juliagraves@Julias-MBP.hsd1.dc.comcast.net>
2017-06-09 16:07:54 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
